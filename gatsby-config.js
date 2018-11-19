e371a716cfcd564f85f479f3df4fcc65225d02d5
Keith Alva <FAKEFUNQ@NESS-PX.lan1>
2018-11-19 15:37:21 -0800

    test!

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Keith+Code!',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Keith is learning to design and code React apps.</h1>
+    <p>Keith is learning about the best tools and design systems. Let's prototype and build something great!</p>
+    <Link to="/page-2/">This is a test! yay!</Link>
+  </div>
+)
+
+export default IndexPage
