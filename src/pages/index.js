a9497de2d4797f72ee108e9906f04fc73c5fd1df
Keith Alva <FAKEFUNQ@NESS-PX.lan1>
2018-11-19 15:48:37 -0800

    Testing 1234

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Keith is learning to design and code React apps.</h1>
     <p>Keith is learning about the best tools and design systems. Let's prototype and build something great!</p>
+    <p> This sentence was made with love! This sentence was used to test. hehehe</p>
     <Link to="/page-2/">This is a test! yay!</Link>
   </div>
 )
