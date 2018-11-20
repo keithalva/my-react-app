2a0c3f94cb6d9eaac2664562b6952aee3254dd98
Keith Alva <FAKEFUNQ@NESS-PX.lan1>
2018-11-19 17:54:24 -0800

    Update on my so super hella sick design

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Keith is learning to design and code React apps.</h1>
-    <p>Keith is learning about the best tools and design systems. Let's prototype and build something great!</p>
-    <p> This sentence was made with love! This sentence was used to test. hehehe</p>
-    <Link to="/page-2/">This is a test! yay!</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Keith is learning to design and code React apps.</h1>
+      <p>Keith is learning about the best tools and design systems.</p>
+      <Link to="/page-2/">This is a test! yay!</Link>
+      </div>
+    </div>
   </div>
 )
 
